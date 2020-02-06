4143b0b56851eed6630d53acf317cd417db2e793
pkt0831 <pkt0831@naver.com>
2020-02-07 01:25:08 +0900

    2.6수정

--- js/main.js
+++ js/main.js
@@ -1,31 +1,81 @@
-var menu = $('.menu');
-var items = $('.menu-item');
-var icons = $('.submenu a');
-var panels = $('.board section');
-var tabs = $('.tab a');
-icons.attr('class', 'icon-dot-circled');
+let menu = $('.quick-booking');
+let items = $('.airline-tab, .hotel-tab, .rentacar-tab');
 
-icons.on('mouseover focusin', function(e){
-	$(this).removeClass('icon-dot-circled');
-	$(this).addClass('icon-ok');
-});
-icons.on('mouseout focusout', function(e){
-	$(this).removeClass('icon-ok');
-	$(this).addClass('icon-dot-circled');
-});
-items.on('click keyup focus', function(e){
-	if (
-		e.type === 'click' ||
-		(e.type === 'keyup' && e.keyCode === 13) ||
-		e.type === 'focus'
-	) {
-		items.removeClass('menu-act');
-		$(this).addClass('menu-act');
-	}
+items.on('click keyup', function(e) {
+    e.preventDefault();
+    if (e.type === 'click' || (e.type === 'keyup' && e.keyCode === 13) ) {
+        items.removeClass('on');
+        $(this).addClass('on');
+        $(this).attr('tabindex', 0).siblings().attr('tabindex', -1);
+    }
 });
 
-tabs.click(function(e){
-	e.preventDefault();
-	panels.removeClass('board-act');
-	$(this).parent().parent().addClass('board-act');
+
+let tabs = $('.quick-booking-tab [role="tab"]')
+
+tabs.on('click keyup', function (e) {
+    // e.preventDefault();
+    if (e.type === 'click' || (e.type === 'keyup' && e.keyCode === 13) ) {
+    $(this).attr('aria-selected', true).siblings().attr('aria-selected', false);
+    let selectedId = "#"+$(this).attr('aria-controls');
+    $(selectedId).addClass('detail-view').siblings().removeClass('detail-view');
+    $(selectedId).attr('tabindex', 0).siblings().attr('tabindex', -1);
+    }
+    
+});
+
+
+
+var button = $('.btn');
+
+button.on('click keyup', function(e) {
+  if (e.type === 'click' || (e.type === 'keyup' && e.keyCode === 13) ) {
+    e.preventDefault();
+      $(this).closest('.btn-grp').children('.btn-on').removeClass('btn-on');
+      $(this).addClass('btn-on');
+      // $(this).addClass('btn-on').siblings().removeClass('btn-on');
+  }
+});
+
+
+
+var booking_detail_menu = $('.airline-ticket, .airline-checkin,.airline-schedule');
+    
+booking_detail_menu.on('click keyup focus', function(e) {
+    if (e.type === 'click' || (e.type === 'keyup' && e.keyCode === 13) || e.type === 'focus') {
+        // e.preventDefault();
+        booking_detail_menu.removeClass('menu-act');
+        $(this).addClass('menu-act');
+    }
+});
+
+
+
+
+
+
+// 메뉴리스트 클릭했을때 서브메뉴 나오게하기
+var submenu_tab = $('.submenu-tab');
+var submenu_tabpanel = $('.submenu-tabpanel');
+
+submenu_tab.on('click keyup', function(e) {
+  e.preventDefault();
+
+  if (e.type === 'click' || (e.type === 'keyup' && e.keyCode === 13) || e.type === 'focus') {
+      submenu_tabpanel.removeClass('submenu-act');
+      $(this).closest('.submenu-tab').next().addClass('submenu-act');
+  }
+});
+
+
+// 클로즈 버튼 눌렀을때 서브메뉴 사라지게 하기
+var close_btn = $('.menu-sub-close-btn');
+
+close_btn.on('click keyup', function(e) {
+  e.preventDefault();
+
+  if (e.type === 'click' || (e.type === 'keyup' && e.keyCode === 13) || e.type === 'focus') {
+      // submenu_tabpanel.removeClass('submenu-act');
+      $(this).closest('.submenu-tabpanel').removeClass('submenu-act');
+  }
 });
